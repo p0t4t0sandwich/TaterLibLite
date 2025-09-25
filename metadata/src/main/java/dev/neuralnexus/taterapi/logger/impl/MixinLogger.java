a037f84ec72106eadf4beab1f4ff597e3b26b83d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-09-25 00:00:59 -0600

    More formatting

--- metadata/src/main/java/dev/neuralnexus/taterapi/logger/impl/MixinLogger.java
+++ metadata/src/main/java/dev/neuralnexus/taterapi/logger/impl/MixinLogger.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2025 Dylan Sperrer - dylan@neuralnexus.dev
+ * This project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLibLite/blob/main/LICENSE">MIT</a>
+ */
 package dev.neuralnexus.taterapi.logger.impl;
 
 import dev.neuralnexus.taterapi.logger.Logger;
