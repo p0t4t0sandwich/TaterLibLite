ad3befd68d79868127c67393a8e943d429eb9801
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-10-12 17:14:14 -0600

    Changed package to "loader"

--- core/src/main/java/dev/neuralnexus/taterapi/loader/EntrypointLoader.java
+++ core/src/main/java/dev/neuralnexus/taterapi/loader/EntrypointLoader.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 Dylan Sperrer - dylan@neuralnexus.dev
  * This project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLibLite/blob/main/LICENSE">MIT</a>
  */
-package dev.neuralnexus.taterapi.plugin;
+package dev.neuralnexus.taterapi.loader;
 
 import dev.neuralnexus.taterapi.meta.Constraint;
 import dev.neuralnexus.taterapi.meta.Constraints;
