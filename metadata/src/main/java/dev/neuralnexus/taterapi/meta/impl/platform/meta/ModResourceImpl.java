abe6db76dde69d68e43f11b136be5a482db0a41d
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-12-03 00:18:51 -0600

    Added catch for when the modloader already has a filesystem open for the jar

--- metadata/src/main/java/dev/neuralnexus/taterapi/meta/impl/platform/meta/ModResourceImpl.java
+++ metadata/src/main/java/dev/neuralnexus/taterapi/meta/impl/platform/meta/ModResourceImpl.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileStore;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemAlreadyExistsException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
@@ -23,11 +24,11 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 public class ModResourceImpl implements ModResource {
-    private final Supplier<Path> pathSupplier;
-    private FSWrapper fileSystem;
+    private final @NonNull Supplier<@NonNull Path> pathSupplier;
+    private FileSystem fileSystem;
     private int refCount = 0;
 
-    public ModResourceImpl(final Supplier<Path> pathSupplier) {
+    public ModResourceImpl(final @NonNull Supplier<@NonNull Path> pathSupplier) {
         this.pathSupplier = pathSupplier;
     }
 
@@ -38,19 +39,30 @@ public class ModResourceImpl implements ModResource {
 
     @Override
     public @NonNull FileSystem fileSystem() throws IOException {
-        this.refCount++;
-        if (this.fileSystem == null || !this.fileSystem.isOpen()) {
-            URI uri = URI.create("jar:" + this.path().toUri() + "!/");
-            this.fileSystem = new FSWrapper(FileSystems.newFileSystem(uri, Collections.emptyMap()));
+        if (this.fileSystem != null && this.fileSystem.isOpen()) {
+            this.refCount++;
+            return this.fileSystem;
         }
+
+        URI uri = URI.create("jar:" + this.path().toUri() + "!/");
+        try {
+            this.fileSystem = new FSWrapper(FileSystems.newFileSystem(uri, Collections.emptyMap()));
+        } catch (FileSystemAlreadyExistsException e) {
+            this.fileSystem = FileSystems.getFileSystem(uri);
+        }
+
+        this.refCount = 1;
         return this.fileSystem;
     }
 
     @Override
     public void close() throws Exception {
         this.refCount--;
-        if (this.refCount <= 0 && this.fileSystem != null && this.fileSystem.isOpen()) {
-            this.fileSystem.closeDelegate();
+        if (this.refCount <= 0
+                && this.fileSystem != null
+                && this.fileSystem.isOpen()
+                && this.fileSystem instanceof FSWrapper fsWrapper) {
+            fsWrapper.closeDelegate();
         }
     }
 
