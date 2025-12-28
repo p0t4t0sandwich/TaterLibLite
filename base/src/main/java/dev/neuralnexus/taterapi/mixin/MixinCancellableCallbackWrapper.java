5fd59f17911781ed8b1c279cd50b71baf86bdceb
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-12-28 03:25:23 -0600

    Added Cancellable Mixin callback wrapper

---
+++ base/src/main/java/dev/neuralnexus/taterapi/mixin/MixinCancellableCallbackWrapper.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2025 Dylan Sperrer - dylan@neuralnexus.dev
+ * This project is Licensed under <a href="https://github.com/p0t4t0sandwich/TaterLibLite/blob/main/LICENSE">MIT</a>
+ */
+package dev.neuralnexus.taterapi.mixin;
+
+import dev.neuralnexus.taterapi.Wrapped;
+import dev.neuralnexus.taterapi.event.Cancellable;
+
+/**
+ * Wrapper for mixin events that implement {@link
+ * org.spongepowered.asm.mixin.injection.callback.Cancellable}.
+ */
+public class MixinCancellableCallbackWrapper
+        implements Cancellable,
+                Wrapped<org.spongepowered.asm.mixin.injection.callback.Cancellable> {
+    private final org.spongepowered.asm.mixin.injection.callback.Cancellable ci;
+
+    public MixinCancellableCallbackWrapper(
+            org.spongepowered.asm.mixin.injection.callback.Cancellable ci) {
+        this.ci = ci;
+    }
+
+    @Override
+    public boolean cancelled() {
+        return this.ci.isCancelled();
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        if (cancelled) {
+            this.ci.cancel();
+        }
+    }
+
+    @Override
+    public org.spongepowered.asm.mixin.injection.callback.Cancellable unwrap() {
+        return this.ci;
+    }
+}
