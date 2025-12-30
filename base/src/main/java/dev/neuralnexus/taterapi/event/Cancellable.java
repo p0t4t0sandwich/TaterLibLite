5e69273cc1eef780abea6917628bc1422f1f03d0
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-12-29 23:27:08 -0600

    Renamed CancellableMixin and added Cancellable.Dummy for multi-use scenarios

--- base/src/main/java/dev/neuralnexus/taterapi/event/Cancellable.java
+++ base/src/main/java/dev/neuralnexus/taterapi/event/Cancellable.java
@@ -6,6 +6,8 @@ package dev.neuralnexus.taterapi.event;
 
 /** Abstract class for cancellable events. */
 public interface Cancellable {
+    Dummy DUMMY = new Dummy();
+
     /**
      * Gets whether the event is cancelled.
      *
@@ -23,5 +25,15 @@ public interface Cancellable {
     /** Cancels the event. */
     default void cancel() {
         this.setCancelled(true);
+    }
+
+    final class Dummy implements Cancellable {
+        @Override
+        public boolean cancelled() {
+            return false;
+        }
+
+        @Override
+        public void setCancelled(boolean cancelled) {}
     }
 }
